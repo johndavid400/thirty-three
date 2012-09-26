aeacdc057ab1a6685a9c7a679c34cba79658c723
johndavid400 <johndavid400@gmail.com>
2012-09-25 22:52:20 -0500

    stuff

--- 1/code/Infrared_control/IRstring_test/IRstring_test.ino
+++ 1/code/Infrared_control/IRstring_test/IRstring_test.ino
@@ -1,13 +1,12 @@
 // This code is intended to decode an Infrared remote using a standard Infrared receiver  from Radio Shack
 // This code is tested on an Arduino Mega
-// Connect the IR detector input pin to Arduino D
+// Connect the IR detector input pin to Arduino D12
 // JDW 2012
 
 int ledPin = 13; // optional LED on pin 13
 int pulse_pin = 12; // connect IR receiver - I used pin 21, but you can change this if using a regular Arduino, any pin will work.
 int pulse_val = 0; // create a variable for the pulse values from the IR detector
 boolean reading = false; // a variable to check and see if the last available pulse was recently
-int loop_counter = 0; // counter to check loop cycles
 String IRstring = ""; // initiate an empty String object
 
 void setup() {   
@@ -26,12 +25,8 @@ void pulse(){
 
 void booleanize(){
   // this function changes the pulse readings of 500 microseconds and 1100 microseconds into 0 or 1 boolean values.
-  if (pulse_val > 750){
-    pulse_val = 1;
-  }
-  else {
-    pulse_val = 0;
-  }
+  if (pulse_val > 750){pulse_val = 1;}
+  else {pulse_val = 0;}
 }
 
 void loop() {
@@ -42,7 +37,6 @@ void loop() {
     reading = true; // assert that we are still reading signals
     booleanize(); // convert each pulse into a boolean 0 or 1
     IRstring = IRstring + pulse_val; // append each new value to a string data object instead of an array
-    loop_counter = 0; // since we just read a pulse, reset the pulse counter to 0
   }
   // if the pulse is not greater than 0...
   else {
@@ -52,11 +46,6 @@ void loop() {
       IRstring = "";  // set the string object to be empty ""
     }
     reading = false; // since we are no longer reading a pulse anymore, set the reading value to false
-    loop_counter++; // increment the loop counter
-    // check to see if the loop count has reached 50 without receiving a signal
-    if (loop_counter > 50){
-      loop_counter = 0; // if so, reset the counter
-    }
   }
   digitalWrite(ledPin, LOW); // turn off the LED
 }
