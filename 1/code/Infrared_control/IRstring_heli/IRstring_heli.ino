6cec0e3c939ab2d97582291b1e6c52b99f1b6302
johndavid400 <johndavid400@gmail.com>
2012-09-29 10:44:45 -0500

    update code

--- 1/code/Infrared_control/IRstring_heli/IRstring_heli.ino
+++ 1/code/Infrared_control/IRstring_heli/IRstring_heli.ino
@@ -10,6 +10,14 @@ boolean reading = false; // a variable to check and see if the last available pu
 int loop_counter = 0; // counter to check loop cycles
 String IRstring = ""; // initiate an empty String object
 
+int m1_A = 8;     //define I1 interface
+int m1_B = 11;    //define I2 interface 
+int m1_pwm = 9;   //enable motor A
+
+int m2_A = 12;    //define I3 interface 
+int m2_B = 13;    //define I4 interface 
+int m2_pwm = 10;  //enable motor B
+
 void setup() {   
   // start serial monitor at 9600 bits per second
   Serial.begin(9600);
@@ -17,6 +25,13 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   // input pulse signal from infrared remote
   pinMode(pulse_pin, INPUT);
+  // Create motor outputs
+  pinMode(m1_A, OUTPUT);
+  pinMode(m1_B, OUTPUT);
+  pinMode(m1_pwm, OUTPUT);
+  pinMode(m2_A, OUTPUT);
+  pinMode(m2_B, OUTPUT);
+  pinMode(m2_pwm, OUTPUT);  
 }
 
 void pulse(){
@@ -35,7 +50,7 @@ void booleanize(){
 }
 
 void loop() {
-   pulse(); // get a pulse reading from the IR sensor
+  pulse(); // get a pulse reading from the IR sensor
   // now check to see if it is a pulse was read
   if (pulse_val > 0){
     digitalWrite(ledPin, HIGH); // when a pulse is being read, turn on the LED on pin 13
@@ -59,6 +74,33 @@ void loop() {
     }
   }
   digitalWrite(ledPin, LOW); // turn off the LED
+
+
+  switch (8) {
+  case '010':    
+    digitalWrite(2, HIGH);
+    break;
+  case 'b':    
+    digitalWrite(3, HIGH);
+    break;
+  case 'c':    
+    digitalWrite(4, HIGH);
+    break;
+  case 'd':    
+    digitalWrite(5, HIGH);
+    break;
+  case 'e':    
+    digitalWrite(6, HIGH);
+    break;
+  default:
+    // turn all the LEDs off:
+    for (int thisPin = 2; thisPin < 7; thisPin++) {
+      digitalWrite(thisPin, LOW);
+    }
+  } 
+
+
 }
 
 
+ 
